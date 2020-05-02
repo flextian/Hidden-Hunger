a9b668e45b50ad66ff44228d1ec2c9358ee5fc27
alex t <sloopooicecream@gmail.com>
2020-05-02 02:34:13 -0400

    added zooming feature based on distance in miles

--- classes/mapscreen.py
+++ classes/mapscreen.py
@@ -1,17 +1,20 @@
 from kivy.uix.screenmanager import Screen
-from kivy_garden.mapview import MapView
 import pgeocode
-
 
 class MapScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def on_enter(self, *args):
+        self.map = self.manager.get_screen("map_screen").ids.map
         self.set_map_lat_lon()
+        self.set_zoom()
 
     def get_zipcode(self):
         return self.manager.get_screen("main_screen").ids.zip_code.text
+
+    def get_zoom(self):
+        return self.manager.get_screen("main_screen").ids.distance.text
 
     def set_map_lat_lon(self):
         zip = self.get_zipcode()
@@ -20,4 +23,18 @@ class MapScreen(Screen):
         longitude = float(zip_to_coords.query_postal_code(zip).get('longitude'))
 
         # Setting the latitude and longitude on the map
-        self.manager.get_screen("map_screen").ids.map.center_on(latitude, longitude)
+        self.map.center_on(latitude, longitude)
+
+    def set_zoom(self):
+        miles_away = self.get_zoom()
+        if miles_away == 'All':
+            self.map.zoom = 6
+        else:
+            if int(miles_away) == 5:
+                self.map.zoom = 12
+            elif int(miles_away) == 15:
+                self.map.zoom = 10
+            elif int(miles_away) == 30:
+                self.map.zoom = 9
+            else:
+                self.map.zoom = 8
