6399d9a36e35f3ea392a874a91b85cc5f0a9f89b
alex t <sloopooicecream@gmail.com>
2020-05-09 15:39:20 -0400

    made the main screen a lot more pretty, need to get the dropdown to create itself only once, not everytime the dropdown is pressed

--- classes/mainscreen.py
+++ classes/mainscreen.py
@@ -1,8 +1,18 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.menu import MDDropdownMenu
 
 
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    # TODO: Make it so that a new dropdown is not made everytime it is pressed! Its slow!
+    def open_dropdown(self):
+        print(self.ids.distance)
+        menu_items = [{"icon": "git", "text": f"Item {i}"} for i in range(5)]
+        self.dropdown = MDDropdownMenu(
+            caller=self.ids.distance, items=menu_items, width_mult=4
+        )
+        self.dropdown.open()
--- classes/resultsscreen.py
+++ classes/resultsscreen.py
@@ -1,10 +1,11 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.menu import MDDropdownMenu
 
 
 class ResultsScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
+
     def go_back(self):
         self.manager.current = 'main_screen'
