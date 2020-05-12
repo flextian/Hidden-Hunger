466da39a3eaa0ec2b4f79fbe77b5e23321d94ce3
alex t <sloopooicecream@gmail.com>
2020-05-12 01:30:22 -0400

    created the dropdown menu properly in the main screen

--- classes/mainscreen.py
+++ classes/mainscreen.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.menu import MDDropdownMenu
@@ -7,12 +8,21 @@ from kivymd.uix.menu import MDDropdownMenu
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.dropdown_menu = None
+        Clock.schedule_once(lambda _: self.create_dropdown_menu())
 
-    # TODO: Make it so that a new dropdown is not made everytime it is pressed! Its slow!
-    def open_dropdown(self):
-        print(self.ids.distance)
-        menu_items = [{"icon": "git", "text": f"Item {i}"} for i in range(5)]
-        self.dropdown = MDDropdownMenu(
-            caller=self.ids.distance, items=menu_items, width_mult=4
+    def create_dropdown_menu(self):
+        menu_items = [{"text": "All"}, {"text": "50"}, {"text": "30"}, {"text": "15"}, {"text": "10"}, {"text": "5"}]
+        self.dropdown_menu = MDDropdownMenu(
+            caller=self.ids.distance, items=menu_items, width_mult=4, callback=self.set_item
         )
-        self.dropdown.open()
+        print('dropdown created!')
+
+    def set_item(self, instance):
+        self.ids.distance.set_item(instance.text)
+        Clock.schedule_once(lambda _: self.dropdown_menu.dismiss(), 0.3)
+
+
+    def open_dropdown(self):
+        print('pressed!')
+        self.dropdown_menu.open()
