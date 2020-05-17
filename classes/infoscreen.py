7ca002396bb18760860db7839a4621b298123dd8
alex t <sloopooicecream@gmail.com>
2020-05-17 18:40:49 -0400

    added white go back button in the info screen, figured out how to change the info screen title on enter

--- classes/infoscreen.py
+++ classes/infoscreen.py
@@ -1,6 +1,11 @@
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.boxlayout import MDBoxLayout
 
 
 class InfoScreen(Screen):
+
     def on_enter(self, *args):
-        pass
+        self.ids.center_panel.ids.title_box.ids.title.text = "title changed"
+
+    def go_back(self):
+        self.manager.current = 'results_screen'
