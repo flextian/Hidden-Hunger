bb9643d2964321e81dd9b2967cb751e5704cc813
alex t <sloopooicecream@gmail.com>
2020-05-17 02:26:48 -0400

    made an invisible button in the MDCard so you are able to click it, made the name of the foodbank centered

--- classes/resultsscreen.py
+++ classes/resultsscreen.py
@@ -1,21 +1,24 @@
-from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 import mysql.connector
 from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
-from kivymd.uix.menu import MDDropdownMenu
 
 
 class FoodbankIcon(MDCard):
-    def __init__(self, name, **kwargs):
+    def __init__(self, info, **kwargs):
         super().__init__(**kwargs)
-        title = MDLabel(text=name)
-        self.add_widget(title)
 
+        title = MDLabel(text=info[1], font_style='H3', halign='center')
+        background_button = Button(background_color=[0,0,0,0])
 
+        anchor = AnchorLayout()
+        anchor.add_widget(background_button)
+        anchor.add_widget(title)
+
+        self.add_widget(anchor)
 
 
 class ResultsScreen(Screen):
@@ -26,6 +29,7 @@ class ResultsScreen(Screen):
         self.search()
 
     def search(self, *args):
+        self.ids.bank_icons.clear_widgets()
         connection = mysql.connector.connect(
             host="foodbanks.cnwqm1nc27hx.us-east-2.rds.amazonaws.com",
             database='foodbanks',
@@ -38,7 +42,7 @@ class ResultsScreen(Screen):
         records = cursor.fetchall()
         for row in records:
             print(row)
-            card = FoodbankIcon(row[1])
+            card = FoodbankIcon(row)
             self.ids.bank_icons.add_widget(card)
 
     def go_back(self):
