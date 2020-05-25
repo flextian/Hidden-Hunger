dd6ffe7f60b1f56c0719816d9b57b8dcac2a358d
alex t <sloopooicecream@gmail.com>
2020-05-25 02:18:45 -0400

    implimented popup for phone number in the info screen

--- classes/infoscreen.py
+++ classes/infoscreen.py
@@ -1,7 +1,16 @@
+from kivy.core.clipboard import Clipboard
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 import webbrowser
+
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.snackbar import Snackbar
+
 from classes.components.marker import Marker
+
+
 # TODO: Fix the flickering in the map
 
 
@@ -26,12 +35,33 @@ class InfoScreen(Screen):
         self.map = None
         self.marker = None
 
+        self.phone_dialog = MDDialog(
+            title="Phone Number",
+            text="",
+            buttons=[
+                MDFlatButton(
+                    text="Close",
+                    text_color=MDApp.get_running_app().theme_cls.primary_color,
+                    on_release=self.close_phone_number_dialog
+                ),
+                MDFlatButton(
+                    text="Copy",
+                    text_color=MDApp.get_running_app().theme_cls.primary_color,
+                    on_release=self.copy_phone_number
+                ),
+            ],
+        )
+
     def on_enter(self, *args):
         self.info = MDApp.get_running_app().row
         self.ids.center_panel.ids.title_box.ids.title.text = self.info[1]
         self.ids.center_panel.ids.list_box.ids.address.text = self.info[2]
         self.ids.center_panel.ids.information.text = self.info[14]
         self.ids.center_panel.ids.title_box.ids.distance.text = str(self.info[15]) + " Miles Away"
+
+        self.phone_dialog.text = self.info[3]
+        self.phone_dialog.size_hint_x = 0.8
+        self.phone_dialog.size = 1, 200
 
         self.map = self.ids.map
         self.marker = Marker(float(self.info[12]), float(self.info[13]), self.map)
@@ -64,9 +94,15 @@ class InfoScreen(Screen):
         webbrowser.open(self.url)
 
     def call_number(self):
-        print('calling')
+        self.phone_dialog.open()
+
+    def copy_phone_number(self, _):
+        Clipboard.copy(self.info[3])
+        Snackbar(text="Copied to Clipboard!").show()
+
+    def close_phone_number_dialog(self, _):
+        self.phone_dialog.dismiss()
 
     def go_back(self):
         self.map.remove_widget(self.marker)
         self.manager.current = 'results_screen'
-
