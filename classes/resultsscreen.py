c752e9fc0290942cf6c8417781097ed93ce98511
alex t <sloopooicecream@gmail.com>
2020-06-10 02:49:35 -0400

    Revert "updated venv"
    This reverts commit 5e76de46bc07b32dae2e2981ebd139953cc5ea4a.

--- classes/resultsscreen.py
+++ classes/resultsscreen.py
@@ -1,7 +1,4 @@
 from kivy.uix.screenmanager import Screen
-from kivymd.theming import ThemableBehavior
-from kivymd.uix.behaviors import RectangularElevationBehavior
-from kivymd.uix.boxlayout import MDBoxLayout
 
 
 class ResultsScreen(Screen):
@@ -12,11 +9,3 @@ class ResultsScreen(Screen):
     def go_back(self):
         self.ids.bank_icons.clear_widgets()
         self.manager.current = "main_screen"
-
-
-class CustomToolbar(
-    ThemableBehavior, RectangularElevationBehavior, MDBoxLayout,
-):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.md_bg_color = self.theme_cls.primary_color
